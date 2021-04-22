2994d18aa8341d54a008f300c199842957caac41
jarvis-byte-github <arkamazumder0@gmail.com>
2021-04-22 16:38:59 +0530

    Eight Commit. News Modification (22-04-2021)

--- app/src/main/java/com/example/covid_19/Fragments/SymptomsFragment.java
+++ app/src/main/java/com/example/covid_19/Fragments/SymptomsFragment.java
@@ -1,9 +1,11 @@
 package com.example.covid_19.Fragments;
 
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -11,13 +13,17 @@ import com.example.covid_19.R;
 
 
 public class SymptomsFragment extends Fragment {
+    TextView txt_know;
 
-
+    //"What you need to know\n
+//
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_vaccination, container, false);
+
+
         return view;
     }
 }
